c6d40d58be1c5e23d74b78f4c9ee09bc42fc2996
=hanchuanchuan <hanchuanchuan@shsnc.com>
2018-12-11 17:59:07 +0800

    代码无更新

--- 3.py
+++ 3.py
@@ -1,16 +1,26 @@
+# -*- coding: utf-8 -*-
+
 import os
 import subprocess
+import argparse
+import sys
 
 # print(os.system("whoami"))
 
 # a = subprocess.call("whoami")
 # print(a)
 
-stdout = subprocess.check_output("whoami",
-                                 stderr=subprocess.STDOUT)
+# stdout = subprocess.check_output("whoami",
+#                                  stderr=subprocess.STDOUT)
 
-stdout = stdout.decode('utf-8').strip()
+# stdout = stdout.decode('utf-8').strip()
 
-print(stdout)
+# print(stdout)
 
 # https://python3-cookbook.readthedocs.io/zh_CN/latest/c13/p06_executing_external_command_and_get_its_output.html
+if __name__ == '__main__':
+
+    print(sys.stdout.encoding)
+    a = input("请输入你的姓名:")
+    print(type(a))
+    print("欢迎:", a)
