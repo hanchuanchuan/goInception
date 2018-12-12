0a207162f082c9bcc0e4bdb44eaa1f9051330b18
=hanchuanchuan <hanchuanchuan@shsnc.com>
2018-12-12 18:28:43 +0800

    测试

--- 4.py
+++ 4.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import time
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+    print("操作开始")
+    time.sleep(10)
+    print("操作继续")
     return 'Hello, World!'
 
 
--- 6.py
+++ 6.py
@@ -3,9 +3,6 @@
 import requests
 
 if __name__ == '__main__':
-    res = requests.get("http://127.0.0.1:5000")
+    res = requests.get("http://127.0.0.1:5000", timeout=1)
     print(res.text)
     # print(res.json())
-
-
-INTRODUCTION
