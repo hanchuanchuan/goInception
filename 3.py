36840047e1ea986d72c33a7646cb32b1e0bd973a
=hanchuanchuan <hanchuanchuan@shsnc.com>
2018-12-20 18:23:20 +0800

    完善审核规则

--- 3.py
+++ 3.py
@@ -5,22 +5,22 @@ import subprocess
 import argparse
 import sys
 
-# print(os.system("whoami"))
 
-# a = subprocess.call("whoami")
-# print(a)
+os.system("whoami")
 
-# stdout = subprocess.check_output("whoami",
-#                                  stderr=subprocess.STDOUT)
 
-# stdout = stdout.decode('utf-8').strip()
+a = subprocess.call("whoami")
+print(a, 123)
 
-# print(stdout)
+a = subprocess.check_output("whoami",
+                            stderr=subprocess.STDOUT)
+a = a.decode('utf-8').strip()
+print(a)
 
 # https://python3-cookbook.readthedocs.io/zh_CN/latest/c13/p06_executing_external_command_and_get_its_output.html
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    print(sys.stdout.encoding)
-    a = input("请输入你的姓名:")
-    print(type(a))
-    print("欢迎:", a)
+#     print(sys.stdout.encoding)
+#     a = input("请输入你的姓名:")
+#     print(type(a))
+#     print("欢迎:", a)
