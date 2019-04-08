6b7c64d4635cc16c728525c6fdf3045095c54d09
hanchuanchuan <hanchuanchuan@gmail.com>
2019-04-08 15:09:37 +0800

    test: 移除mysql驱动升级不再使用的参数

--- server/statistics_handler_test.go
+++ server/statistics_handler_test.go
@@ -100,7 +100,7 @@ func (ds *testDumpStatsSuite) prepareData(c *C) {
 func (ds *testDumpStatsSuite) checkData(c *C, path string) {
 	db, err := sql.Open("mysql", getDSN(func(config *mysql.Config) {
 		config.AllowAllFiles = true
-		config.Strict = false
+		// config.Strict = false
 	}))
 	c.Assert(err, IsNil, Commentf("Error connecting"))
 	dbt := &DBTest{c, db}
