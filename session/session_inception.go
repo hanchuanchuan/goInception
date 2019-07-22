6e3364adb924b9cfae5b0b5e834c0b1b13b3975d
zhaoshun <996846239@qq.com>
2019-07-22 11:58:03 +0800

    修复 alter table 命令没有其他选项时能正常通过的bug

--- session/session_inception.go
+++ session/session_inception.go
@@ -3098,6 +3098,9 @@ func (s *session) checkAlterTable(node *ast.AlterTableStmt, sql string) {
 	for i, alter := range node.Specs {
 		switch alter.Tp {
 		case ast.AlterTableOption:
+			if len(alter.Options) == 0 {
+				s.AppendErrorNo(ER_NOT_SUPPORTED_ALTER_OPTION)
+			}
 			s.checkTableOptions(alter.Options, node.Table.Name.String(), false)
 		case ast.AlterTableAddColumns:
 			s.checkAddColumn(table, alter)
