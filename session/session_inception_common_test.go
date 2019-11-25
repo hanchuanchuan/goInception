9d20f06fae007bbf1aee609965b236e2bb611fdb
hanchuanchuan <chuanchuanhan@gmail.com>
2019-11-25 16:33:03 +0800

    test: 修改默认的测试数据库

--- session/session_inception_common_test.go
+++ session/session_inception_common_test.go
@@ -98,7 +98,7 @@ func (s *testCommon) initSetUp(c *C) {
 	inc := &config.GetGlobalConfig().Inc
 
 	inc.BackupHost = "127.0.0.1"
-	inc.BackupPort = 3307
+	inc.BackupPort = 3306
 	inc.BackupUser = "test"
 	inc.BackupPassword = "test"
 
