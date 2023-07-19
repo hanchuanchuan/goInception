cc0826dec4a2832f26d4df0a626683386992f86c
guoguangwu <guoguangwu@magic-shield.com>
2023-07-19 16:20:21 +0800

    chore: slice append replace loop

--- store/mockstore/mocktikv/executor.go
+++ store/mockstore/mocktikv/executor.go
@@ -566,9 +566,7 @@ func (e *topNExec) evalTopN(value [][]byte) error {
 	}
 
 	if e.heap.tryToAddRow(newRow) {
-		for _, val := range value {
-			newRow.data = append(newRow.data, val)
-		}
+		newRow.data = append(newRow.data, value...)
 	}
 	return errors.Trace(e.heap.err)
 }
