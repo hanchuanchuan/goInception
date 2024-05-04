c7448eb877e7dda2ed026e37944558cdf1ae1d77
hanchuanchuan <chuanchuanhan@gmail.com>
2024-05-04 10:29:56 +0800

    fix ci error

--- executor/write_test.go
+++ executor/write_test.go
@@ -961,8 +961,8 @@ func (s *testSuite) TestUpdate(c *C) {
 	tk.MustExec("create table update_test(id int not null auto_increment, name varchar(255), index(id))")
 	tk.MustExec("insert into update_test(name) values ('aa')")
 	_, err := tk.Exec("update update_test set id = null where name = 'aa'")
-	c.Assert(err, IsNil)
-	// c.Assert(err.Error(), DeepEquals, "[table:1048]Column 'id' cannot be null")
+	c.Assert(err, NotNil)
+	c.Assert(err.Error(), DeepEquals, "[table:1048]Column 'id' cannot be null")
 
 	tk.MustExec("drop table update_test")
 	tk.MustExec("create table update_test(id int)")
